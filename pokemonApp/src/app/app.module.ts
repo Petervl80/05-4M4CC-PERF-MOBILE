619c11c513b41b8685775c629ed7b421f04cd0fa
Petervl80 <pedro.lapa1911@gmail.com>
2023-11-05 23:58:01 -0300

    Atualização

--- pokemonApp/src/app/app.module.ts
+++ pokemonApp/src/app/app.module.ts
@@ -14,4 +14,6 @@ import { AppComponent } from './app.component';
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  
+}
