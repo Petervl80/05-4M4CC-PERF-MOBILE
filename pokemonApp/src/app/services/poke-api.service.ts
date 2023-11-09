b106a7960ed1d3f9e512cf7985b087c1fece84dd
Petervl80 <pedro.lapa1911@gmail.com>
2023-11-08 23:54:39 -0300

    Lista realizada

--- pokemonApp/src/app/services/poke-api.service.ts
+++ pokemonApp/src/app/services/poke-api.service.ts
@@ -4,10 +4,10 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ViaCEPService {
+export class PokeAPIService {
 
   constructor(private httpClient: HttpClient) { }
-  getViaCEPService(cep: string = '52011210') {
-    return this.httpClient.get(`https://viacep.com.br/ws/${cep}/json/`)
+  getPokeAPIService(id: number = Math.floor(Math.random() * 100)) {
+    return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
   }
 }
